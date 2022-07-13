fae30181defa415fc556910eb9e2923db83fd82a
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2022-07-13 18:14:19 +0100

    build script to run on BBN OS to build APM Planner 2.0 for raspberry pi
    build script to run on BBN OS to build APM Planner 2.0 for raspberry pi

--- build-rpi.sh
+++ build-rpi.sh
@@ -2,3 +2,19 @@
 
 # This is build script to run on BBN OS to build APM Planner 2.0 for raspberry pi
 # https://github.com/bareboat-necessities/lysmarine_gen
+
+sudo apt-get install qt5-qmake qt5-default qtscript5-dev libqt5webkit5-dev libqt5serialport5-dev  \
+ libqt5svg5-dev libqt5serialport5-dev libqt5opengl5-dev qml-module-qtquick-controls libsndfile1-dev devscripts
+
+cd ~
+
+git clone https://github.com/diydrones/apm_planner && cd apm_planner/debian
+
+./gitlog-to-changelog.pl > changelog
+
+debuild -us -uc -d
+
+ls -l ~/apmplanner2*.deb
+
+
+
