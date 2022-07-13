32625fe64fa5497e5dc9175cd53c33b0a06207a8
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2022-07-13 18:27:38 +0100

    install script to run on BBN OS for APM Planner 2.0 for raspberry pi
    install script to run on BBN OS for APM Planner 2.0 for raspberry pi

---
+++ install-rpi.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+
+sudo apt-get install qt5-default qtscript5 libqt5webkit5 libqt5serialport5  \
+ libqt5svg5 libqt5serialport5 libqt5opengl5 qml-module-qtquick-controls libsndfile1
+ 
+sudo dpg -i ~/apmplanner2_2.0.29-rc1-16-g339533bfb_armhf.deb
