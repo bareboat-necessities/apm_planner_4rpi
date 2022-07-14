c170d79df670086449892420f68eff56de0687c9
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2022-07-13 22:46:12 -0400

    Update install-rpi.sh

--- install-rpi.sh
+++ install-rpi.sh
@@ -3,4 +3,4 @@
 sudo apt-get install qt5-default qtscript5 libqt5webkit5 libqt5serialport5  \
  libqt5svg5 libqt5serialport5 libqt5opengl5 qml-module-qtquick-controls libsndfile1
  
-sudo dpg -i ~/apmplanner2_2.0.29-rc1-16-g339533bfb_armhf.deb
+sudo dpkg -i ~/apmplanner2_2.0.29-rc1-16-g339533bfb_armhf.deb
