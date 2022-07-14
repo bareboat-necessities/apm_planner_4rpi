e0bb71713468fae1e8809548ad382470988e149f
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2022-07-13 22:50:32 -0400

    Update install-rpi.sh

--- install-rpi.sh
+++ install-rpi.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
+# This is a script to install APM Planner 2.0 for raspberry pi on BBN OS  
+# https://github.com/bareboat-necessities/lysmarine_gen
+
 sudo apt-get install -y libqt5quickwidgets5 libqt5webkit5 libqt5serialport5 \
  libqt5svg5 libqt5serialport5 libqt5opengl5 libqt5test5 qml-module-qtquick-controls libsndfile1
 
