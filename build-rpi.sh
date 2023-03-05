3e479d1302f2a89c8b216a31a71f5404ac810f6a
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-05 02:59:17 +0000

    Update build-rpi.sh

--- build-rpi.sh
+++ build-rpi.sh
@@ -3,12 +3,13 @@
 # This is build script to run on BBN OS to build APM Planner 2.0 for raspberry pi
 # https://github.com/bareboat-necessities/lysmarine_gen
 
-sudo apt-get install qt5-qmake qt5-default qtscript5-dev libqt5webkit5-dev libqt5serialport5-dev  \
+
+sudo apt-get -y install qt5-qmake qtbase5-dev qtchooser qtbase5-dev-tools qtscript5-dev libqt5webkit5-dev libqt5serialport5-dev  \
  libqt5svg5-dev libqt5serialport5-dev libqt5opengl5-dev qml-module-qtquick-controls libsndfile1-dev devscripts
 
 cd ~
 
-git clone https://github.com/diydrones/apm_planner && cd apm_planner/debian
+git clone https://github.com/diydrones/apm_planner && mv apm_planner apmplanner2 && cd apmplanner2/debian
 
 ./gitlog-to-changelog.pl > changelog
 
