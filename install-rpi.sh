d705410cc754289771301fe3e7aa3951de015bae
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-05 05:10:11 +0000

    Update install-rpi.sh

--- install-rpi.sh
+++ install-rpi.sh
@@ -7,6 +7,6 @@ sudo apt-get install -y libqt5quickwidgets5 libqt5webkit5 libqt5serialport5 \
  libqt5svg5 libqt5serialport5 libqt5opengl5 libqt5test5 qml-module-qtquick-controls libsndfile1
 
 # https://github.com/ArduPilot/apm_planner
-wget https://github.com/bareboat-necessities/apm_planner_4rpi/releases/download/v2.0.29-rc1-16-g339533bfb/apmplanner2_2.0.29-rc1-16-g339533bfb_armhf.deb
-sudo dpkg -i apmplanner2_2.0.29-rc1-16-g339533bfb_armhf.deb 
-rm -f apmplanner2_2.0.29-rc1-16-g339533bfb_armhf.deb
+wget https://github.com/bareboat-necessities/apm_planner_4rpi/releases/download/v2.0.29-rc1-36-g1fba2b8fc/apmplanner2_2.0.29-rc1-36-g1fba2b8fc_arm64.deb
+sudo dpkg -i apmplanner2_2.0.29-rc1-36-g1fba2b8fc_arm64.deb 
+rm -f apmplanner2_2.0.29-rc1-36-g1fba2b8fc_arm64.deb
